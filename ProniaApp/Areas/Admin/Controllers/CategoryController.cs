446faae2ccb88a8a1938778083adad79dd8594cb
Aysun12-i <aysunisk06@gmail.com>
2026-05-12 20:08:51 +0400

    CRUD

--- ProniaApp/Areas/Admin/Controllers/CategoryController.cs
+++ ProniaApp/Areas/Admin/Controllers/CategoryController.cs
@@ -23,18 +23,18 @@ namespace ProniaApp.Areas.Admin.Controllers
 
         public IActionResult Create()
         {
-
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> Create(Category category)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return View(category);
             }
-            bool result = await _context.Categories.AnyAsync(c => c.Name==category.Name);
+
+            bool result = await _context.Categories.AnyAsync(c => c.Name.Trim().ToLower() == category.Name.Trim().ToLower());
 
             if (result)
             {
@@ -42,21 +42,81 @@ namespace ProniaApp.Areas.Admin.Controllers
                 return View(category);
 
             }
-            
+
             await _context.Categories.AddAsync(category);
             await _context.SaveChangesAsync();
 
             return RedirectToAction("Index");
         }
+
         public IActionResult Detail(int? id)
         {
-            if (id == null) return NotFound();
+            if (id == null || id <= 0) return BadRequest();
 
-            Category category = _context.Categories.Include(c => c.Products) .FirstOrDefault(c => c.Id == id);
+            Category category = _context.Categories.Include(c => c.Products).FirstOrDefault(c => c.Id == id);
 
             if (category == null) return NotFound();
 
             return View(category);
         }
+
+
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id <= 0) return BadRequest();
+
+            Category existed = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (existed == null) return NotFound();
+
+            return View(existed);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, Category category)
+        {
+            if (id == null || id <= 0) return BadRequest();
+
+            Category existed = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (existed == null) return NotFound();
+
+
+            if (!ModelState.IsValid)
+            {
+                return View(existed);
+            }
+
+            bool result = await _context.Categories.AnyAsync(c => c.Name.Trim().ToLower() == category.Name.Trim().ToLower() && c.Id != id);
+
+            if (result)
+            {
+                ModelState.AddModelError("Name", "This category already exists");
+                return View(existed);
+            }
+
+            existed.Name = category.Name;
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id <= 0) return BadRequest();
+
+            Category existed = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (existed == null) return NotFound();
+
+            _context.Categories.Remove(existed);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
     }
 }
